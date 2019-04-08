1ea10fa888c0d0bf8f5532d7eaf67e4dfd628409
UmarBilanov <30313763+UmarBilanov@users.noreply.github.com>
2019-04-08 12:12:09 +0600

    Add files via upload

---
+++ src/android/NewPlugin.java
@@ -0,0 +1,28 @@
+package com.example.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.widget.Toast;
+import android.content.Context;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+public class NewPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("runBackground")) {
+            Context context = cordova.getActivity().getApplicationContext();
+            Intent service = new Intent(context, MyService.class);
+            context.startService(service);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
